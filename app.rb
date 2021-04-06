0c59736db41939d91bd5d57bb6418e2af1adb76f
Adam Walker <adamw207@yahoo.com>
2021-04-06 19:38:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
+
+
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    user_word = params["user_phrase"]
+    word = PigLatinizer.new
+    @pig_latinized_word = word.to_pig_latin(user_word)
+
+    erb :pig_latin
+  end
 end
